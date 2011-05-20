a789988e848a840c4b38dba71c40cd84811724a2
Sun Liwen <sunliwen@gmail.com>
2011-05-20 10:44:40 +0800

    Add log_uploader (copy log to hdfs)

---
+++ BatchServer/log_uploader/log_uploader.py
@@ -0,0 +1,16 @@
+import os
+
+
+LOG_FILE_ROOT = "logs"
+
+
+# 1. check each customer directory
+# 2. if there is a current file and also other files.
+# 3. copy other files to hdfs, move them to trash bin. 
+for customer_directory in os.listdir(LOG_FILE_ROOT):
+    cd_path = LOG_FILE_ROOT + "/" + customer_directory
+    file_names = os.listdir(cd_path)
+    if not "ROTATING" in file_names:
+        if "current" in file_names:
+            file_names.remove("current")
+        os.system("hadoop dfs -copyFromLocal %s %s" % (local_path, remote_path))
