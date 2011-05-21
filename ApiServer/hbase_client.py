5e7e88387c6f4564586abeac0d465b00b9250749
Sun Liwen <sunliwen@gmail.com>
2011-05-21 15:25:47 +0800

    1. Add another guess you like based on browser history recommendation algorithm 2. use corresponding table to recommend for different customer site.

--- ApiServer/hbase_client.py
+++ ApiServer/hbase_client.py
@@ -17,7 +17,6 @@ protocol = TBinaryProtocol.TBinaryProtocol(transport)
 
 client = Hbase.Client(protocol)
 
-ITEM_SIMILARITY_TABLE = "demo1_item_similarities"
 
 def doHash(id):
     return md5.md5(id).hexdigest()
@@ -28,11 +27,12 @@ def updateItem(item):
     item_json = json.dumps(item)
     client.mutateRow("items", the_id, [Mutation(column="p:content", value=item_json)])
 
+def _getItemSimilaritiesTable(customer_id):
+    return "%s_item_similarities" % customer_id
+
 def recommend_viewed_also_view(customer_id, item_id, amount):
-    # FIXME: we ignore customer_id currently
-    row = client.getRow(ITEM_SIMILARITY_TABLE, doHash(item_id))
-    #item_id1 = row[0].columns["p:item_id1"].value
-    #print "ITEM_ID1:", item_id1
+    tableName = _getItemSimilaritiesTable(customer_id)
+    row = client.getRow(tableName, doHash(item_id))
     if len(row) == 0:
         return []
     most_similar_items = json.loads(row[0].columns["p:mostSimilarItems"].value)
@@ -52,12 +52,13 @@ def sign(float):
         return -1
 
 
-def recommend_based_on_browsing_history(customer_id, pref_ids, amount):
+def calc_weighted_top_list_method1(customer_id, pref_ids):
     if len(pref_ids) > 10:
         recent_pref_ids = pref_ids[-10:]
     else:
         recent_pref_ids = pref_ids
 
+    # calculate weighted top list from recent browsing history
     rec_map = {}
     for pref_id in recent_pref_ids:
         recommended_items = recommend_viewed_also_view(customer_id, str(pref_id), 15)
@@ -71,7 +72,37 @@ def recommend_based_on_browsing_history(customer_id, pref_ids, amount):
         score_total, count = rec_map[key][0], rec_map[key][1]
         rec_tuples.append((key, score_total / count))
     rec_tuples.sort(lambda a,b: sign(b[1] - a[1]))
-    topn = [int(rec_tuple[0]) for rec_tuple in rec_tuples]
+    return [int(rec_tuple[0]) for rec_tuple in rec_tuples]
+
+
+def calc_weighted_top_list_method2(customer_id, pref_ids):
+    # see "Programming Collective Intelligence P25"
+    # FIXME: seems not work well for implicit rating?
+    if len(pref_ids) > 10:
+        recent_pref_ids = pref_ids[-10:]
+    else:
+        recent_pref_ids = pref_ids
+
+    # calculate weighted top list from recent browsing history
+    scores = {}
+    totalSim = {}
+    for pref_id in recent_pref_ids:
+        recommended_items = recommend_viewed_also_view(customer_id, str(pref_id), 15)
+        for rec_item, score in recommended_items:
+            if int(rec_item) not in pref_ids:
+                scores.setdefault(rec_item, 0)
+                scores[rec_item] += float(score)
+                totalSim.setdefault(rec_item, 0)
+                totalSim[rec_item] += float(score)
+    rankings = [(score/totalSim[item], item) for item, score in scores.items()]
+    print rankings
+    rankings.sort()
+    rankings.reverse()
+    return [ranking[1] for ranking in rankings]    
+
+
+def recommend_based_on_browsing_history(customer_id, pref_ids, amount):
+    topn = calc_weighted_top_list_method1(customer_id, pref_ids) 
     if len(topn) > amount:
         topn = topn[:amount]
     return topn
