91eee99d2161fb8b4f6b22672d20cedaed38bc72
Sun Liwen <sunliwen@gmail.com>
2011-06-07 15:40:58 +0800

    API Server: add the import_items.py .

---
+++ ApiServer/scripts/import_items.py
@@ -0,0 +1,25 @@
+# coding=utf-8
+import sys
+sys.path.insert(0, "../")
+sys.path.insert(0, "../../")
+import csv
+import mongo_client
+
+
+site_id = sys.argv[1]
+csv_file = sys.argv[2]
+
+f = open("goods_list.csv", "r")
+
+reader = csv.reader(f)
+fields = reader.next()
+count = 0
+for row in csv.DictReader(f, fieldnames=fields):
+    count += 1
+    if count % 1000 == 0:
+        print count
+    item = {"item_id": row["id"], "item_name": row["商品名称"], 
+              "price": row["商品价格"], "image_link": row["商品缩略图"], 
+              "description": row["商品详情"],
+              "item_link": "http://ecshop.tuijianbao.net/goods.php?id=%s" % row["id"]}
+    mongo_client.updateItem(site_id, item)
