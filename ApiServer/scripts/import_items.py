c87d85b5a78908a3226351a41a99cc5daf1fb6db
Sun Liwen <sunliwen@gmail.com>
2011-06-08 15:36:18 +0800

    change import script to relative path

--- ApiServer/scripts/import_items.py
+++ ApiServer/scripts/import_items.py
@@ -21,5 +21,5 @@ for row in csv.DictReader(f, fieldnames=fields):
     item = {"item_id": row["id"], "item_name": row["商品名称"], 
               "price": row["商品价格"], "image_link": row["商品缩略图"], 
               "description": row["商品详情"],
-              "item_link": "http://ecshop.tuijianbao.net/goods.php?id=%s" % row["id"]}
+              "item_link": "goods.php?id=%s" % row["id"]}
     mongo_client.updateItem(site_id, item)
