0b4c5dd4f0393880db1dfdeab8b60631219a42c6
Sun Liwen <sunliwen@gmail.com>
2011-06-12 18:56:15 +0800

    benchmark_mongo.py updated.

--- ApiServer/benchmark_mongo.py
+++ ApiServer/benchmark_mongo.py
@@ -4,12 +4,24 @@ import time
 import pymongo
 
 
-connection = pymongo.Connection()
+connection = pymongo.Connection(port=9999)
+connection.drop_database("benchmarkdb1")
+connection["benchmarkdb1"].create_collection("coll", {"capped":True, "size": 102400000})
 collection = connection["benchmarkdb1"]["coll"]
-collection.ensure_index([("number", pymongo.DESCENDING)])
+#collection.ensure_index([("number", pymongo.DESCENDING)])
+
 
 t1 = time.time()
 for number in range(50000):
     collection.insert({"number": number, "name": "NAME:%s" % number})
+t2 = time.time()
+print t2 - t1
+
+
+t1 = time.time()
+count = 0
+for row in collection.find().sort("$natural", -1):
+    count += 1
+    if count > 5000: break
 t2 = time.time()
 print t2 - t1
