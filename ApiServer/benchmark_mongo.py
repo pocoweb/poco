d2de84cdd5a7dbc3648cac74456b5219e7f2fcc6
Sun Liwen <sunliwen@gmail.com>
2011-06-12 18:21:05 +0800

    benchmark_mongo updated.

--- ApiServer/benchmark_mongo.py
+++ ApiServer/benchmark_mongo.py
@@ -1,22 +1,15 @@
 import sys
 sys.path.insert(0, "../")
 import time
-from common.utils import getSiteDBCollection
 import pymongo
 
 
 connection = pymongo.Connection()
-item_similarities = getSiteDBCollection(connection, "demo2", "raw_logs")
-t1 = time.time()
-for i in xrange(10000):
-    item_similarities.find_one()
-t2 = time.time()
-print t2 - t1
+collection = connection["benchmarkdb1"]["coll"]
+collection.ensure_index([("number", pymongo.DESCENDING)])
 
 t1 = time.time()
-count = 0
-for row in item_similarities.find().sort("timestamp", -1):
-    count += 1
-    if count > 10000: break
+for number in range(50000):
+    collection.insert({"number": number, "name": "NAME:%s" % number})
 t2 = time.time()
 print t2 - t1
