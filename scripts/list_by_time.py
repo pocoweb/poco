65ba790570afb784562ba49f1d3cde5f5af8df1e
Sun Liwen <sunliwen@gmail.com>
2011-08-10 06:06:33 +0800

    improve list_by_time

--- scripts/list_by_time.py
+++ scripts/list_by_time.py
@@ -19,8 +19,12 @@ conn = pymongo.Connection("cube2")
 def datetime2ts(datetime):
     return time.mktime(datetime.timetuple())
 
-d0 = datetime.datetime(2011,8,9,0,0,0)
+today = datetime.datetime.today()
+d0 = datetime.datetime(today.year,today.month,today.day,0,0,0)
 for i in range(24):
     d1 = d0 + datetime.timedelta(minutes=60 * i)
     d2 = d0 + datetime.timedelta(minutes=60 * (i+1))
-    print "%s(%s) - %s" % (d1, datetime2ts(d1), d2), conn["tjbsite_"+host].raw_logs.find({"timestamp":{"$gt":datetime2ts(d1), "$lt":datetime2ts(d2)}}).count()
+    print "%s(%s) - %s" % (d1, datetime2ts(d1), d2), 
+    print conn["tjbsite_"+host].raw_logs.find({"timestamp":{"$gt":datetime2ts(d1), "$lt":datetime2ts(d2)}}).count(),
+    print conn["tjbsite_"+host].raw_logs.find({"timestamp":{"$gt":datetime2ts(d1), "$lt":datetime2ts(d2)}, "behavior": "V"}).count(),
+    print conn["tjbsite_"+host].raw_logs.find({"timestamp":{"$gt":datetime2ts(d1), "$lt":datetime2ts(d2)}, "behavior": "PLO"}).count()
