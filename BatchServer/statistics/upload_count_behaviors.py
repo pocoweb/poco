f319780f756fe535e2253dd766c97ca0c585ed16
Sun Liwen <sunliwen@gmail.com>
2011-08-11 13:20:23 +0800

    BatchServer: Bug fix. revert back changes to BatchServer/statistics/upload_count_behaviors.py.

--- BatchServer/statistics/upload_count_behaviors.py
+++ BatchServer/statistics/upload_count_behaviors.py
@@ -6,19 +6,20 @@ def upload_count_behaviors(connection, site_id, input_path):
         count, rest = line.strip().split()
         count = int(count)
         behavior, date_str = rest.split(",")
-        c_statistics = getSiteDBCollection(connection, site_id, "statistics")
-        content_row = {
-                "date": date_str,
-                "PV_V": 0,
-                "PV_Rec": 0,
-                "PV_PLO": 0,
-                "ClickRec": 0}
+        statistics = getSiteDBCollection(connection, site_id, "statistics")
+        row_in_db = statistics.find_one({"date": date_str})
+        if row_in_db is None:
+            row_in_db = {"date": date_str}
+        row_in_db.setdefault("PV_V", 0)
+        row_in_db.setdefault("PV_Rec", 0)
+        row_in_db.setdefault("PV_PLO", 0)
+        row_in_db.setdefault("ClickRec", 0)
         if behavior == "V":
-            content_row["PV_V"] = count
+            row_in_db["PV_V"] = count
         elif behavior == "PLO":
-            content_row["PV_PLO"] = count
+            row_in_db["PV_PLO"] = count
         elif behavior.startswith("Rec"):
-            content_row["PV_Rec"] = count
+            row_in_db["PV_Rec"] += count
         elif behavior == "ClickRec":
-            content_row["ClickRec"] = count
-        c_statistics.update({"date": date_str}, {"$set": content_row}, upsert=True)
+            row_in_db["ClickRec"] = count
+        statistics.save(row_in_db)
