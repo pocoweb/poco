ca1f1e91fc0a2a313d6327052aaea729fc1d843c
Sun Liwen <sunliwen@gmail.com>
2011-10-08 18:24:48 +0800

    modify datepicker style

--- Dashboard/static/dashboard.js
+++ Dashboard/static/dashboard.js
@@ -40,6 +40,14 @@ $(document).ready(function(){
     current: new Date(current_month),
 		mode: 'range',
     starts: 1,
+    locale: {
+      days: ["日", "一", "二", "三", "四", "五", "六", "日"],
+      daysShort: ["日", "一", "二", "三", "四", "五", "六", "日"],
+      daysMin: ["日", "一", "二", "三", "四", "五", "六", "日"],
+      months: ["1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月", "10月", "11月", "12月"],
+      monthsShort: ["1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月", "10月", "11月", "12月"],
+      weekMin: '周'
+    },
     onChange: function(formated) {
       console.log(formated);
 			$('#widgetField span').get(0).innerHTML = formated.join(' -- ');
