7f98d66981083256305f97019403c39f30a87323
Sun Liwen <sunliwen@gmail.com>
2011-11-03 16:24:11 +0800

    add local_settings template

--- Dashboard/gunicorn.conf.py
+++ Dashboard/gunicorn.conf.py
@@ -1,3 +1,3 @@
-bind = "unix:/tmp/beta.tuijianbao.net.sock"
-logfile = "/cube/app/beta.tuijianbao.net/logs/gunicorn_dashboard.log"
+bind = "unix:/tmp/tuijianbao.net.sock"
+logfile = "/cube/app/tuijianbao.net/logs/gunicorn_dashboard.log"
 workers = 1
