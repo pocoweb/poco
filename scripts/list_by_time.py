54b45dc6d74e3528a295d3d9671edf2b7d897d1b
Sun Liwen <sunliwen@gmail.com>
2011-11-12 01:37:41 +0800

    timestamp to created_on

--- scripts/list_by_time.py
+++ scripts/list_by_time.py
@@ -14,10 +14,10 @@ except getopt.GetoptError, err:
 
 print host
 
-conn = pymongo.Connection("cube13")
+conn = pymongo.Connection("cube11")
 
 def datetime2ts(datetime):
-    return time.mktime(datetime.timetuple())
+    return datetime
 
 today = datetime.datetime.today()
 d0 = datetime.datetime(today.year,today.month,today.day,0,0,0)
@@ -25,6 +25,6 @@ for i in range(24):
     d1 = d0 + datetime.timedelta(minutes=60 * i)
     d2 = d0 + datetime.timedelta(minutes=60 * (i+1))
     print "%s(%s) - %s" % (d1, datetime2ts(d1), d2), 
-    print conn["tjbsite_"+host].raw_logs.find({"timestamp":{"$gt":datetime2ts(d1), "$lt":datetime2ts(d2)}}).count(),
-    print conn["tjbsite_"+host].raw_logs.find({"timestamp":{"$gt":datetime2ts(d1), "$lt":datetime2ts(d2)}, "behavior": "V"}).count(),
-    print conn["tjbsite_"+host].raw_logs.find({"timestamp":{"$gt":datetime2ts(d1), "$lt":datetime2ts(d2)}, "behavior": "PLO"}).count()
+    print conn["tjbsite_"+host].raw_logs.find({"created_on":{"$gt":datetime2ts(d1), "$lt":datetime2ts(d2)}}).count(),
+    print conn["tjbsite_"+host].raw_logs.find({"created_on":{"$gt":datetime2ts(d1), "$lt":datetime2ts(d2)}, "behavior": "V"}).count(),
+    print conn["tjbsite_"+host].raw_logs.find({"created_on":{"$gt":datetime2ts(d1), "$lt":datetime2ts(d2)}, "behavior": "PLO"}).count()
